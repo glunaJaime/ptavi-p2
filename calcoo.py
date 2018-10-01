744eabc3d475c8cb84681801660d4911f45c8efc
Jaime Garcia <luna@MacBook-Pro-de-Jaime.local>
2018-10-01 10:38:27 +0200

    calcoo.py terminado

---
+++ calcoo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class calcoo(): 
+	""" Creo la clase calculadora """
+	def suma(self, op1, op2)
+		return op1 + op2
+	def resta(self, op1, op2)
+		return op1 - op2
+
+if __name__=="__main__":
+	try:
+		operando1 = int(sys.arg[1])
+		operando2 = int(sys,argv[3])
+	except ValueError:
+		sys.exit("Error: Non numercial paramter")
+
+	calcoo = Calcoo()
+
+	if sys.argv[2] == "suma":
+		result = calcoo.suma(operando1,operando2)
+	elif sys.argv[2] == "resta":
+		result = calcoo.resta(operando1, operando2)
+	else:
+		sys.exit ('operacion solo puede ser suma o resta')
+ 	print (result)
