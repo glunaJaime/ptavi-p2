7109910167312c5cdf2a274f43c4c3f26cdd64c2
Jaime Garcia <luna@MacBook-Pro-de-Jaime.local>
2018-10-01 15:58:22 +0200

    practica 2 con pep8 comlpeta

--- calcoo.py
+++ calcoo.py
@@ -8,26 +8,26 @@ class calculadora():
     """ Creo la clase calculadora """
 
 
-def suma(self, op1, op2):
-    return op1 + op2
+    def plus(self, op1, op2):
+        return op1 + op2
 
 
-def resta(self, op1, op2):
-    return op1 - op2
+    def min(self, op1, op2):
+        return op1 - op2
 
 if __name__ == "__main__":
     try:
-        operando1 = int(sys.arg[1])
+        operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
     except ValueError:
         sys.exit("Error: Non numercial paramter")
 
-    calculadora = Calculadora()
+    calculadora = calculadora()
 
     if sys.argv[2] == "suma":
-        result = calculadora.suma(operando1, operando2)
+        result = calculadora.plus(operando1, operando2)
     elif sys.argv[2] == "resta":
-        result = calculadora.resta(operando1, operando2)
+        result = calculadora.min(operando1, operando2)
     else:
         sys.exit('operacion solo puede ser suma o resta')
     print(result)
